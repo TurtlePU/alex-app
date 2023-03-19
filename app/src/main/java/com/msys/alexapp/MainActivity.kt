90cd25c61f473ce596b6b85370d277e838555f3e
TurtlePU <sokolov.p64@gmail.com>
2023-03-19 11:35:18 +0300

    + NavComposable

--- app/src/main/java/com/msys/alexapp/MainActivity.kt
+++ app/src/main/java/com/msys/alexapp/MainActivity.kt
@@ -6,10 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.msys.alexapp.components.Authorization
+import com.msys.alexapp.components.Carousel
 import com.msys.alexapp.ui.theme.AlexAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +23,7 @@ class MainActivity : ComponentActivity() {
       AlexAppTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+          NavComposable()
         }
       }
     }
@@ -27,14 +31,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun NavComposable() {
+  val navController = rememberNavController()
+  NavHost(navController = navController, startDestination = "authorization") {
+    composable("authorization") {
+      Authorization { userID -> navController.navigate("carousel/$userID") }
+    }
+    composable("carousel/{userID}") { backStackEntry ->
+      Carousel(backStackEntry.arguments!!.getString("userID")!!)
+    }
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
   AlexAppTheme {
-    Greeting("Android")
+    NavComposable()
   }
 }
---
+++ app/src/main/java/com/msys/alexapp/components/Carousel.kt
@@ -0,0 +1,8 @@
+package com.msys.alexapp.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Carousel(userID: String) {
+  TODO("Not yet implemented")
+}
