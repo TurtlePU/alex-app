fa572c4fe4260ad866f1aef8a0b9d79bf20896c8
TurtlePU <sokolov.p64@gmail.com>
2023-04-13 12:44:48 +0300

    + [friends] choosing friends

--- app/src/main/java/com/msys/alexapp/services/FriendRepo.kt
+++ app/src/main/java/com/msys/alexapp/services/FriendRepo.kt
@@ -1,14 +1,30 @@
 package com.msys.alexapp.services
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.snapshots
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.tasks.asDeferred
+import kotlinx.coroutines.tasks.await
 
-private val email: String get() = FirebaseAuth.getInstance().currentUser!!.email!!
+private fun invitations(email: String, uid: String? = null): DatabaseReference =
+  FirebaseDatabase.getInstance()
+    .getReference("invitations/$email")
+    .run { uid?.let { child(it) } ?: this }
+
+private fun friends(uid: String): DatabaseReference =
+  FirebaseDatabase.getInstance().getReference("data/$uid/friends")
 
 fun friendIdsByRole(role: String): Flow<List<String>> =
-  FirebaseDatabase.getInstance().getReference("invitations/$email").snapshots.map { data ->
-    data.children.filter { it.value == role }.map { it.key!! }
-  }
+  invitations(FirebaseAuth.getInstance().currentUser!!.email!!)
+    .snapshots.map { data -> data.children.filter { it.value == role }.map { it.key!! } }
+
+suspend fun chooseFriends(myRole: String, emails: List<String>) {
+  val uid = FirebaseAuth.getInstance().uid!!
+  val friendTask = friends(uid).setValue(emails.associateWith { "" })
+  emails.map { email -> invitations(email, uid).setValue(myRole).asDeferred() }.awaitAll()
+  friendTask.await()
+}
