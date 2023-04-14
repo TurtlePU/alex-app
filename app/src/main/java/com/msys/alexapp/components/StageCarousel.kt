5a0fed8f2d003fcfbfe81ce9cd708885f28c08f9
TurtlePU <sokolov.p64@gmail.com>
2023-04-14 15:34:24 +0300

    stage carousel ver.0.1

--- app/src/main/java/com/msys/alexapp/components/StageCarousel.kt
+++ app/src/main/java/com/msys/alexapp/components/StageCarousel.kt
@@ -1,9 +1,20 @@
 package com.msys.alexapp.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.NavigateNext
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.msys.alexapp.R
 import com.msys.alexapp.data.Advice
 import com.msys.alexapp.data.Performance
 import kotlinx.coroutines.flow.Flow
+import java.util.*
 
 interface StageService {
   val firstStagedPerformance: Flow<Pair<Long, Performance>?>
@@ -12,4 +23,33 @@ interface StageService {
 }
 
 @Composable
-fun StageService.Carousel(finishStage: () ->  Unit) {}
+fun StageService.Carousel(finishStage: () -> Unit) {
+  val performance by firstStagedPerformance.collectAsStateWithLifecycle(initialValue = null)
+  performance?.let {
+    LaunchedEffect(true) { setCurrent(it.second) }
+    val deadline = rememberSaveable { currentDate().time + timeout.inWholeMilliseconds }
+    LaunchedEffect(true) { sendAdvice(Advice(Date(deadline), it.first)) }
+    it.second.View(
+      index = it.first,
+      deadline = Date(deadline),
+      floatingActionButton = {
+        ExtendedFloatingActionButton(
+          text = { /*TODO*/ },
+          icon = {
+            Icon(
+              imageVector = Icons.Filled.NavigateNext,
+              contentDescription = stringResource(R.string.next_performance),
+            )
+          },
+          onClick = { /*TODO*/ },
+        )
+      }
+    ) {
+      /*TODO*/
+    }
+  } ?: FinishStage(finishStage)
+}
+
+@Composable
+fun FinishStage(finishStage: () -> Unit) {
+}
