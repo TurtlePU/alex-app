207b8286373c3b631417a7d6db4e5c8ca944cd0d
TurtlePU <sokolov.p64@gmail.com>
2023-04-14 20:28:53 +0300

    [stage carousel] informational components ver.0.1

--- app/src/main/java/com/msys/alexapp/components/StageCarousel.kt
+++ app/src/main/java/com/msys/alexapp/components/StageCarousel.kt
@@ -1,11 +1,16 @@
 package com.msys.alexapp.components
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.NavigateNext
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -66,17 +71,41 @@ fun StageService.PerformanceDashboard(
         ?.let { id -> NextButton(id, canFinish) { scope.launch { finishPerformance() } } }
         ?: FinishButton(canFinish) { scope.launch { finishPerformance(); finishStage() } }
     }
-  ) {
-    JuryRow(dashboard.values)
-  }
+  ) { JuryRow(dashboard.values) }
 }
 
 @Composable
 fun RatingBar(averageRating: Double) {
+  if (!averageRating.isNaN()) {
+    Row {
+      Text(text = averageRating.toString())
+      Text(text = stringResource(place(averageRating)))
+    }
+  }
+}
+
+@StringRes
+fun place(rating: Double): Int = when {
+  rating >= 9.5 -> R.string.laureate_1
+  rating >= 9.0 -> R.string.laureate_2
+  rating >= 8.5 -> R.string.laureate_3
+  rating >= 8.0 -> R.string.diploma_1
+  rating >= 7.5 -> R.string.diploma_2
+  rating >= 7.0 -> R.string.diploma_3
+  else -> R.string.participant
 }
 
 @Composable
 fun JuryRow(dashboard: Collection<JuryNote>) {
+  Row {
+    for (note in dashboard) {
+      Card {
+        Text(text = note.nickname)
+        Text(text = note.report?.rating?.toString() ?: "")
+        Text(text = note.report?.comment ?: "")
+      }
+    }
+  }
 }
 
 @Composable
