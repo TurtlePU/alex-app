17341d050d38724d4157168c3c30280f30d2ee99
TurtlePU <sokolov.p64@gmail.com>
2023-04-15 12:13:16 +0300

    [performance list] report list v0.1

--- app/src/main/java/com/msys/alexapp/components/PerformanceList.kt
+++ app/src/main/java/com/msys/alexapp/components/PerformanceList.kt
@@ -1,24 +1,26 @@
 package com.msys.alexapp.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Cancel
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Start
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.msys.alexapp.R
 import com.msys.alexapp.components.Tabs.*
@@ -137,18 +139,9 @@ fun StagingList(
 ) {
   LazyColumn(modifier = modifier) {
     items(items = performances, key = { it.id }) {
-      it.run {
-        Row(
-          modifier = Modifier
-            .fillMaxWidth()
-            .clickable { onClick(id) }
-            .background(background(id))
-        ) {
-          Text(text = id)
-          Text(text = name)
-          Text(text = performance)
-        }
-      }
+      it.Card(modifier = Modifier
+        .clickable { onClick(it.id) }
+        .background(background(it.id)))
     }
     item { NewPerformance(newPerformanceInitialID, newPerformance) }
   }
@@ -156,9 +149,64 @@ fun StagingList(
 
 @Composable
 fun ReportList(reports: List<Pair<Performance, StageReport>>, modifier: Modifier = Modifier) {
+  var expandedID: String? by remember { mutableStateOf(null) }
+  val manager = LocalClipboardManager.current
   LazyColumn(modifier = modifier) {
-    items(items = reports, key = { it.first.id }) { (performance, report) ->
+    items(items = reports, key = { it.first.id }) { pair ->
+      pair.Card(
+        onClick = { expandedID = it },
+        isExpanded = { expandedID == it },
+        copyText = manager::setText,
+      )
     }
+  }
+}
+
+@Composable
+fun Pair<Performance, StageReport>.Card(
+  onClick: (String) -> Unit,
+  isExpanded: (String) -> Boolean,
+  copyText: (AnnotatedString) -> Unit,
+) {
+  Column {
+    first.Card(modifier = Modifier.clickable { onClick(first.id) })
+    val transitionState = remember { MutableTransitionState(isExpanded(first.id)) }
+    AnimatedVisibility(visibleState = transitionState) { second.Card(copyText) }
+  }
+}
+
+@Composable
+fun Performance.Card(modifier: Modifier = Modifier) {
+  Row(
+    modifier = Modifier
+      .fillMaxWidth()
+      .then(modifier)
+  ) {
+    Text(text = id)
+    Text(text = name)
+    Text(text = performance)
+  }
+}
+
+@Composable
+fun StageReport.Card(copyText: (AnnotatedString) -> Unit) {
+  Row(modifier = Modifier.fillMaxWidth()) {
+    Text(text = averageRating.toString())
+    val commentText = comments.entries.joinToString(separator = "\n") { it.run { "$key: $value" } }
+    TextField(
+      value = commentText,
+      onValueChange = {},
+      readOnly = true,
+      trailingIcon = {
+        IconButton(onClick = { copyText(AnnotatedString(commentText)) }) {
+          Icon(
+            imageVector = Icons.Filled.CopyAll,
+            contentDescription = stringResource(R.string.copy_comments),
+          )
+        }
+      },
+      singleLine = false,
+    )
   }
 }
 
