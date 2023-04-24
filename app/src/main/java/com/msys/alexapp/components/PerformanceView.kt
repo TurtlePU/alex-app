bd03db9d2b9578560e86ac7f820726603b07dc30
TurtlePU <sokolov.p64@gmail.com>
2023-04-24 21:00:05 +0300

    add glow animation past deadline

--- app/src/main/java/com/msys/alexapp/components/PerformanceView.kt
+++ app/src/main/java/com/msys/alexapp/components/PerformanceView.kt
@@ -1,19 +1,39 @@
 package com.msys.alexapp.components
 
 import android.icu.util.Calendar
-import androidx.compose.foundation.layout.*
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.msys.alexapp.data.Performance
 import kotlinx.coroutines.delay
-import java.util.*
+import java.util.Date
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.minutes
@@ -81,10 +101,33 @@ fun Timeout(deadline: Date) {
       delay(1000.milliseconds)
     }
   }
-  val progress = (1f * (deadline.time - time.time) / timeout.inWholeMilliseconds).coerceIn(0f, 1f)
+  val progress = (1 - 1f * (deadline.time - time.time) / timeout.inWholeMilliseconds)
+    .coerceIn(0f, 1f)
+  val (modifier, color) = if (progress == 1f) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val animatedElevation by infiniteTransition.animateFloat(
+      initialValue = 0f,
+      targetValue = 5f,
+      animationSpec = infiniteRepeatable(
+        animation = tween(
+          easing = FastOutLinearInEasing,
+        ),
+        repeatMode = RepeatMode.Reverse,
+      )
+    )
+    Modifier.shadow(
+      elevation = animatedElevation.dp,
+      ambientColor = Color.Red,
+    ) to Color.Red
+  } else {
+    Modifier to ProgressIndicatorDefaults.linearColor
+  }
   LinearProgressIndicator(
     progress = progress,
-    modifier = Modifier.fillMaxWidth(),
+    modifier = Modifier
+      .fillMaxWidth()
+      .then(modifier),
+    color = color,
   )
 }
 
