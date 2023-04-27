f08076e5a33473df954d4cc9013ca7f7b7c918db
TurtlePU <sokolov.p64@gmail.com>
2023-04-27 19:28:46 +0300

    + CommentBoxPreview

---
+++ app/src/main/java/com/msys/alexapp/components/admin/CommentBox.kt
@@ -0,0 +1,34 @@
+package com.msys.alexapp.components.admin
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.msys.alexapp.R
+
+@Composable
+fun CommentBox(setCanComment: (Boolean) -> Unit) {
+  Row {
+    var canComment by rememberSaveable { mutableStateOf(false) }
+    Checkbox(
+      checked = canComment,
+      onCheckedChange = {
+        canComment = !canComment
+        setCanComment(canComment)
+      },
+    )
+    Text(text = stringResource(R.string.comments))
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CommentBoxPreview() {
+  CommentBox { }
+}
