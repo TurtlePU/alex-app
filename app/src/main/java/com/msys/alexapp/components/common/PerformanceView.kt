6e676d7bb4fdecf6363bd44e38354ab7c1e6a465
TurtlePU <sokolov.p64@gmail.com>
2023-04-28 17:02:14 +0300

    restore old performance view

--- app/src/main/java/com/msys/alexapp/components/common/PerformanceView.kt
+++ app/src/main/java/com/msys/alexapp/components/common/PerformanceView.kt
@@ -1,5 +1,12 @@
 package com.msys.alexapp.components.common
 
+import android.icu.util.Calendar
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
@@ -7,27 +14,29 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.msys.alexapp.data.Performance
-import com.msys.alexapp.ui.theme.AlexAppTheme
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.withContext
 import java.util.Date
-import kotlin.time.Duration.Companion.seconds
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.minutes
 
 @Composable
 fun Performance.View(
@@ -37,70 +46,29 @@ fun Performance.View(
   floatingActionButton: @Composable () -> Unit = {},
   content: @Composable ColumnScope.() -> Unit,
 ) {
-  var progress by rememberSaveable { mutableStateOf(0f) }
-  LaunchedEffect(true) {
-    withContext(Dispatchers.IO) {
-      val timeout = deadline - currentDate()
-      while (true) {
-        val time = currentDate()
-        progress = (1 - (deadline - time) / timeout).coerceIn(.0, .1).toFloat()
-        delay(1.seconds)
-      }
-    }
-  }
   Scaffold(
     topBar = {
       Row(
         modifier = Modifier
           .fillMaxWidth()
-          .padding(10.dp),
+          .padding(vertical = 10.dp),
         verticalAlignment = Alignment.Top,
-        horizontalArrangement = Arrangement.SpaceBetween,
+        horizontalArrangement = Arrangement.SpaceEvenly,
       ) {
+        Text(text = category?.firstWord ?: "")
+        Column {
+          Text(text = nomination?.firstWord ?: "", maxLines = 1)
+          Text(text = age.toString())
+        }
+        Column {
+          Text(text = "(#$id) $name", textAlign = TextAlign.Center, maxLines = 1)
+          Text(text = performance, textAlign = TextAlign.Center, maxLines = 1)
+        }
         Text(
-          text = category?.firstWord ?: "",
-          modifier = Modifier.weight(1f),
-          textAlign = TextAlign.Center,
-          style = MaterialTheme.typography.headlineLarge,
+          text = city ?: "",
+          textAlign = TextAlign.Right
         )
-        Column(
-          modifier = Modifier.weight(1f),
-          horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-          Text(
-            text = nomination?.firstWord ?: "",
-            style = MaterialTheme.typography.displayMedium,
-          )
-          Text(
-            text = age.toString(),
-            style = MaterialTheme.typography.displayMedium,
-          )
-        }
-        Column(
-          modifier = Modifier.weight(3f),
-          horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-          Text(
-            text = "(#$id) $name",
-            maxLines = 1,
-            style = MaterialTheme.typography.displayMedium,
-          )
-          Text(
-            text = performance,
-            maxLines = 1,
-            style = MaterialTheme.typography.displayMedium,
-          )
-        }
-        Column(
-          modifier = Modifier.weight(1f),
-          horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-          cornerButton()
-          Text(
-            text = city ?: "",
-            style = MaterialTheme.typography.displayMedium,
-          )
-        }
+        cornerButton()
       }
     },
     bottomBar = bottomBar,
@@ -110,11 +78,53 @@ fun Performance.View(
       modifier = Modifier
         .fillMaxSize()
         .padding(padding),
+      verticalArrangement = Arrangement.SpaceBetween,
     ) {
-      Timeout(progress)
+      Timeout(deadline)
       content()
     }
   }
+}
+
+fun currentDate(): Date = Calendar.getInstance().time
+operator fun Date.plus(duration: Duration): Date = Date(time + duration.inWholeMilliseconds)
+operator fun Date.minus(other: Date): Duration = (time - other.time).milliseconds
+val defaultTimeout: Duration = 5.minutes
+
+@Composable
+fun Timeout(deadline: Date) {
+  var time by remember { mutableStateOf(currentDate()) }
+  LaunchedEffect(true) {
+    while (true) {
+      time = currentDate()
+      delay(1000.milliseconds)
+    }
+  }
+  val progress = (1 - (deadline - time) / defaultTimeout).coerceIn(.0, .1).toFloat()
+  val infiniteTransition = rememberInfiniteTransition()
+  val animatedElevation by infiniteTransition.animateFloat(
+    initialValue = 0f,
+    targetValue = 5f,
+    animationSpec = infiniteRepeatable(
+      animation = tween(easing = FastOutLinearInEasing),
+      repeatMode = RepeatMode.Reverse,
+    )
+  )
+  val (modifier, color) = if (progress == 1f) {
+    Modifier.shadow(
+      elevation = animatedElevation.dp,
+      ambientColor = Color.Red,
+    ) to Color.Red
+  } else {
+    Modifier to ProgressIndicatorDefaults.linearColor
+  }
+  LinearProgressIndicator(
+    progress = progress,
+    modifier = Modifier
+      .fillMaxWidth()
+      .then(modifier),
+    color = color,
+  )
 }
 
 val String.firstWord: String get() = this.trim().split(' ').first()
@@ -129,8 +139,8 @@ val example = Performance(
   nomination = "song"
 )
 
-@Preview(showBackground = true, device = "spec:width=1280dp,height=800dp,dpi=480")
+@Preview(showBackground = true)
 @Composable
 fun PerformancePreview() {
-  AlexAppTheme { example.View(currentDate()) {} }
+  example.View(deadline = currentDate()) {}
 }
