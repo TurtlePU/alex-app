0f8e13d85b071da4e986da593689da38dd8d43a3
TurtlePU <sokolov.p64@gmail.com>
2023-04-29 11:38:52 +0300

    fix spreadsheet headers

--- app/src/main/java/com/msys/alexapp/tasks/SaveResults.kt
+++ app/src/main/java/com/msys/alexapp/tasks/SaveResults.kt
@@ -2,10 +2,42 @@ package com.msys.alexapp.tasks
 
 import android.os.Environment
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import com.msys.alexapp.R
+import com.msys.alexapp.data.Summary
 import com.msys.alexapp.services.FirebaseService
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.io.Closeable
+import java.io.OutputStream
+
+enum class Columns {
+  ID, NAME, PERFORMANCE, DEGREE, RATING;
+
+  val header
+    get(): @StringRes Int = when (this) {
+      ID -> R.string.id_header
+      NAME -> R.string.name_header
+      PERFORMANCE -> R.string.performance_header
+      DEGREE -> R.string.degree_header
+      RATING -> R.string.rating_header
+    }
+}
+
+operator fun Summary.get(columns: Columns): String = when (columns) {
+  Columns.ID -> id
+  Columns.NAME -> name
+  Columns.PERFORMANCE -> performance
+  Columns.DEGREE -> degree
+  Columns.RATING -> rating.toString()
+}
+
+data class StreamAndHeaders(val stream: OutputStream, val headers: Map<Columns, String>) :
+  Closeable {
+  override fun close() = stream.close()
+}
 
 @Composable
 fun saveResults(): Task = launchedTask(
@@ -13,8 +45,13 @@ fun saveResults(): Task = launchedTask(
     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
   ),
   input = Environment.DIRECTORY_DOCUMENTS ?: "",
-  resource = { LocalContext.current.contentResolver.openOutputStream(it) },
-) { outputStream ->
+  resource = {
+    StreamAndHeaders(
+      LocalContext.current.contentResolver.openOutputStream(it) ?: return@launchedTask null,
+      Columns.values().associateWith { col -> stringResource(col.header) }
+    )
+  },
+) { (outputStream, headers) ->
   val workbook = XSSFWorkbook()
   val sheet = workbook.createSheet()
   var count = 0
@@ -22,14 +59,13 @@ fun saveResults(): Task = launchedTask(
   val results = FirebaseService.collectResults()
   for (result in results) {
     sheet.createRow(++count).run {
-      createCell(0).setCellValue(result.id)
-      createCell(1).setCellValue(result.name)
-      createCell(2).setCellValue(result.performance)
-      createCell(3).setCellValue(result.degree)
-      createCell(4).setCellValue(result.rating)
+      for (column in Columns.values()) {
+        createCell(column.ordinal).setCellValue(result[column])
+      }
+      getCell(Columns.RATING.ordinal).setCellValue(result.rating)
       for ((jury, comment) in result.comments) {
         val column = juryColumns.computeIfAbsent(jury) {
-          juryColumns.maxOfOrNull { it.value + 1 } ?: 5
+          juryColumns.maxOfOrNull { it.value + 1 } ?: Columns.values().size
         }
         createCell(column).setCellValue(comment)
       }
@@ -37,8 +73,9 @@ fun saveResults(): Task = launchedTask(
     send(.3f * count / results.size)
   }
   sheet.createRow(0).run {
-    createCell(0).setCellValue("№")
-    createCell(1).setCellValue("Средняя оценка")
+    for (column in Columns.values()) {
+      createCell(column.ordinal).setCellValue(headers[column])
+    }
     juryColumns.onEachIndexed { index, (jury, col) ->
       createCell(col).setCellValue(jury)
       send(.3f + .3f * index / juryColumns.size)
