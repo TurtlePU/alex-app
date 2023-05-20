54f0848ccd2a913da6f8f4fd390d312bc41a795d
TurtlePU <sokolov.p64@gmail.com>
2023-05-20 14:21:55 +0300

    change degrees

--- app/src/main/java/com/msys/alexapp/data/Degrees.kt
+++ app/src/main/java/com/msys/alexapp/data/Degrees.kt
@@ -2,6 +2,6 @@ package com.msys.alexapp.data
 
 val defaultDegrees = sortedMapOf(
   10.0 to "Л1", 9.49 to "Л2", 8.99 to "Л3",
-  7.99 to "Д1", 6.99 to "Д2", 5.99 to "Д3",
-  5.0 to "УЧ",
+  8.49 to "Д1", 7.99 to "Д2", 7.49 to "Д3",
+  6.99 to "УЧ",
 )
